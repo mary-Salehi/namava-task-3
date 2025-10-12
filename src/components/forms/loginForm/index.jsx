17be041bd33a12005b08538bfbba8ce821e0c471
maryamSalehi <maryam010salehi@gmail.com>
2025-10-12 14:01:17 +0330

    Implemented submit logic

--- src/components/forms/loginForm/index.jsx
+++ src/components/forms/loginForm/index.jsx
@@ -1,19 +1,48 @@
-import { Link } from "react-router-dom";
 import { useStyles } from "./styles";
+import { useState } from "react";
 
 function LoginForm() {
   const classes = useStyles();
+
+  const [userData, setUserData] = useState({
+    phone: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("jdhgjhdg");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className={classes.inputContainer}>
-        <label htmlFor="">شماره تلفن همراه یا ایمیل</label>
-        <input type="text" placeholder="شماره تلفن همراه یا ایمیل" />
+        <label>شماره تلفن همراه یا ایمیل</label>
+        <input
+          type="text"
+          placeholder="شماره تلفن همراه یا ایمیل"
+          value={userData.phone}
+          name="phone"
+          onChange={handleChange}
+        />
       </div>
       <div className={classes.inputContainer}>
-        <label htmlFor="">رمز عبور</label>
-        <input type="text" placeholder="رمز عبور" />
+        <label>رمز عبور</label>
+        <input
+          type="text"
+          placeholder="رمز عبور"
+          value={userData.password}
+          name="password"
+          onChange={handleChange}
+        />
       </div>
-      <Link className={classes.submitBtn}>ورود</Link>
+      <button type="submit" className={classes.submitBtn}>
+        ورود
+      </button>
     </form>
   );
 }
