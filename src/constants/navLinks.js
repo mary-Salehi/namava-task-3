b9ef368b5438a09ff7c470959415f1370678a547
maryamSalehi <maryam010salehi@gmail.com>
2025-10-14 14:29:17 +0330

    Added header navlinks

---
+++ src/constants/navLinks.js
@@ -0,0 +1,42 @@
+export const navLinks = [
+  {
+    id: 1,
+    title: 'خانه',
+    path: '#',
+  },
+  {
+    id: 2,
+    title: 'فیلم‌ها',
+    path: '#',
+  },
+  {
+    id: 3,
+    title: 'سریال ها',
+    path: '#',
+  },
+  {
+    id: 4,
+    title: 'دسته بندی',
+    path: '#',
+  },
+  {
+    id: 5,
+    title: 'تازه ها',
+    path: '#',
+  },
+  {
+    id: 6,
+    title: 'کودکان',
+    path: '#',
+  },
+  {
+    id: 7,
+    title: 'پردیس نماوا',
+    path: '#',
+  },
+  {
+    id: 8,
+    title: 'نماوا مگ',
+    path: '#',
+  },
+]
