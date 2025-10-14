716bb50b1353047fe324b22c05e77bd8f8c412b2
maryamSalehi <maryam010salehi@gmail.com>
2025-10-14 15:09:28 +0330

    Added Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@
 import { Toaster } from "react-hot-toast";
 import LoginPage from "./pages/login";
 import { useGlobalStyles } from "./styles/globals";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/main";
 function App() {
   useGlobalStyles();
 
   return (
     <>
-      <LoginPage/>
       <Toaster />
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Routes>
     </>
   )
 }
