b2143ab13f7738ab714c0c7c350d53fa4099077e
maryamSalehi <maryam010salehi@gmail.com>
2025-10-16 16:24:41 +0330

    Created functions to set and get cookies

---
+++ src/utils/handleCookie.js
@@ -0,0 +1,17 @@
+export function setCookie(name, value, minute = 1, path = "/") {
+  let seconds = minute * 60;
+  document.cookie = `${name}=${value}; max-age=${seconds}; path=${path}`;
+}
+
+export function getCookie(name) {
+  const cookies = document.cookie.split(
+    "; "
+  );
+  for (let cookie of cookies) {
+    cookie = cookie.trim();
+    if (cookie.startsWith(name + "=")) {
+      return cookie.substring(name.length + 1);
+    }
+  }
+  return null;
+}
