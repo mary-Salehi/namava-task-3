095396d0e8d3601721321ab78282115cd185a509
maryamSalehi <maryam010salehi@gmail.com>
2025-10-18 10:50:29 +0330

    Added a  static image to HomePage

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,9 +1,12 @@
 import Header from '../../components/layout/header'
+import { useStyles } from './styles'
 
 function HomePage() {
+  const classes = useStyles()
   return (
     <div>
       <Header/>
+        <img className={classes.img} src="src/assets/images/swiper-img.jpg" alt="image" />
     </div>
   )
 }
