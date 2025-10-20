ddc9fa61e2e3cf9f09bda1bf74d98043efa0a06e
maryamSalehi <maryam010salehi@gmail.com>
2025-10-20 11:42:18 +0330

    Refactored component

--- src/ui/textField/index.jsx
+++ src/ui/textField/index.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import ClosedEye from "../../components/icons/ClosedEye";
 import OpenEye from "../../components/icons/OpenEye";
 import { useStyles } from "./styles";
+import { convertToPersianNumbers } from "../../utils/convertNumbers";
+import { isOnlyNumbers } from "../../utils/validation";
 
 function TextField({
   label,
@@ -14,32 +16,37 @@ function TextField({
   phoneIcon = false,
   passwordIcon = false,
 }) {
-  const classes = useStyles()
-  const [isPasswordShown,setIspasswordShown] = useState(false);
+  const classes = useStyles();
+  const [isPasswordShown, setIspasswordShown] = useState(false);
 
   const handleShowPassword = () => {
-    setIspasswordShown(!isPasswordShown)
-  }
+    setIspasswordShown(!isPasswordShown);
+  };
 
-  const isPhoneNumber = value.length > 8 && value.startsWith('09') && phoneIcon;
-
+  const isPhoneNumber = isOnlyNumbers(value) && phoneIcon;
 
   return (
     <div className={className}>
       <label>{label}</label>
       <div className={classes.inputWrapper} dir="ltr">
-        {isPhoneNumber && <span className={classes.phoneIcon}>+98</span>}
+        {isPhoneNumber && value.length > 8 && (
+          <span className={classes.phoneIcon}>
+            +{convertToPersianNumbers(98)}
+          </span>
+        )}
         <input
-          
           type={passwordIcon ? (isPasswordShown ? "text" : "password") : type}
           placeholder={placeholder}
-          value={value}
+          value={isPhoneNumber ? convertToPersianNumbers(value) : value}
           name={name}
           onChange={onChange}
+          maxLength={isPhoneNumber ? 11 : undefined}
         />
-        {value.length > 0 && passwordIcon && <span onClick={handleShowPassword} className={classes.showPassBtn}>
-          {isPasswordShown ? <ClosedEye/> : <OpenEye/>}
-          </span>}
+        {value.length > 0 && passwordIcon && (
+          <span onClick={handleShowPassword} className={classes.showPassBtn}>
+            {isPasswordShown ? <ClosedEye /> : <OpenEye />}
+          </span>
+        )}
       </div>
     </div>
   );
