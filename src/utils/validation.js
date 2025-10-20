56afc76f80653467fb6090fb76c72d6a40aa127f
maryamSalehi <maryam010salehi@gmail.com>
2025-10-20 11:37:03 +0330

    Added isOnlyNumbers

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,4 +1,5 @@
-import { passwordRegex, usernameRegex } from "../constants/regex";
+import { numbersRegex, passwordRegex, usernameRegex } from "../constants/regex";
 
 export const isValidUsername = (username) => usernameRegex.test(username);
 export const isValidPassword = (password) => passwordRegex.test(password);
+export const isOnlyNumbers = (text) => numbersRegex.test(text);
