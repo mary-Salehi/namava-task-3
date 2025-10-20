513126db604e61f6dafb2afdae018d4217a383a5
maryamSalehi <maryam010salehi@gmail.com>
2025-10-20 11:36:16 +0330

    Added numbersRegex

--- src/constants/regex.js
+++ src/constants/regex.js
@@ -1,2 +1,3 @@
 export const usernameRegex = /^(09\d{9}|\w+@\w+\.\w{2,})$/;
 export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+export const numbersRegex = /^[\d۰۱۲۳۴۵۶۷۸۹0-9]+$/;
