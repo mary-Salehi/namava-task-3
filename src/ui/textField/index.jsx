0a1e35f35791a2a9860c211d7c4ab77f247ea440
maryamSalehi <maryam010salehi@gmail.com>
2025-10-20 08:32:05 +0330

    Added conditional icons

--- src/ui/textField/index.jsx
+++ src/ui/textField/index.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import ClosedEye from "../../components/icons/ClosedEye";
+import OpenEye from "../../components/icons/OpenEye";
+import { useStyles } from "./styles";
+
 function TextField({
   label,
   name,
@@ -6,17 +11,36 @@ function TextField({
   placeholder,
   onChange,
   className,
+  phoneIcon = false,
+  passwordIcon = false,
 }) {
+  const classes = useStyles()
+  const [isPasswordShown,setIspasswordShown] = useState(false);
+
+  const handleShowPassword = () => {
+    setIspasswordShown(!isPasswordShown)
+  }
+
+  const isPhoneNumber = value.length > 8 && value.startsWith('09') && phoneIcon;
+
+
   return (
     <div className={className}>
       <label>{label}</label>
-      <input
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        name={name}
-        onChange={onChange}
-      />
+      <div className={classes.inputWrapper} dir="ltr">
+        {isPhoneNumber && <span className={classes.phoneIcon}>+98</span>}
+        <input
+          
+          type={passwordIcon ? (isPasswordShown ? "text" : "password") : type}
+          placeholder={placeholder}
+          value={value}
+          name={name}
+          onChange={onChange}
+        />
+        {value.length > 0 && passwordIcon && <span onClick={handleShowPassword} className={classes.showPassBtn}>
+          {isPasswordShown ? <ClosedEye/> : <OpenEye/>}
+          </span>}
+      </div>
     </div>
   );
 }
